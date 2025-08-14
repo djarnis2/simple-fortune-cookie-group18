29e1d3348fcad2ae680187334d58de9f23241207
Lars Kristiansen <larkr23@student.sdu.dk>
2025-08-14 17:15:17 +0000

    Delete ci folder, Modify docker-compose.yaml to images, Made docker-compose-dev.yaml for dev (refs #10)

--- ci/build-docker.sh
+++
@@ -1,7 +0,0 @@
-set -e
-[[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT::8}" 
-OWNER_LC="$(echo "$docker_username" | tr '[:upper:]' '[:lower:]')"
-REPO="ghcr.io/${OWNER_LC}"
-IMG="${REPO}/simple-fortune-cookie-group18"
-echo "${REPO}"
-docker build -t "${REPO}simple-fortune-cookie-group18:latest" -t "${REPO}simple-fortune-cookie-group18:1.0-$Tag" .
